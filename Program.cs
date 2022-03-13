6d93d09da0560ed7fd69de524bb7eed5a299badc
Majulia <majulialcarvalho@gmail.com>
2022-03-13 16:52:28 -0300

    adding sum and subtraction

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Globalization;
+using static System.Console;
 
 namespace Calculadora
 {
@@ -6,7 +7,42 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Welcome to my first Calculator!");
+            
+            Sum();
+            Subtraction();
+            
+
+        }
+
+        static void Sum()
+        {
+            WriteLine($"Please, enter with one value: ");
+            
+            float firstValue = float.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            
+            WriteLine($"Now enter with the second value: ");
+            float secondValue = float.Parse(ReadLine(), CultureInfo.InvariantCulture);
+
+            float result = firstValue + secondValue;
+            
+            WriteLine($"The result of the sum is: {result.ToString(CultureInfo.InvariantCulture)}");
+            ReadKey();
+        }
+
+        static void Subtraction()
+        {
+            WriteLine($"Please, enter with one value: ");
+            
+            float firstValue = float.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            
+            WriteLine($"Now enter with the second value: ");
+            float secondValue = float.Parse(ReadLine(), CultureInfo.InvariantCulture);
+
+            float result = firstValue - secondValue;
+            
+            WriteLine($"The result of the subtraction is: {result.ToString(CultureInfo.InvariantCulture)}");
+            ReadKey();
         }
     }
 }
